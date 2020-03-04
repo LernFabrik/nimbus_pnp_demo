ef1d0635514dbe8ca1f0913b54ac652eba1d70e0
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-03-04 16:15:13 +0100

    Correspondence Failed test

--- nimbus_cloud/src/main.cpp
+++ nimbus_cloud/src/main.cpp
@@ -6,13 +6,14 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/io/pcd_io.h>
 #include <boost/foreach.hpp>
+#include <pcl/correspondence.h>
 
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/TransformStamped.h>
 
 #include <nimbus_cloud/cloud_mean.h>
-// #include <nimbus_cloud/cloud_edit.h>
+#include <nimbus_cloud/cloud_edit.h>
 #include <nimbus_cloud/cloud_features.h>
 #include <nimbus_cloud/cloud_keypoints.h>
 
@@ -36,8 +37,10 @@ int main(int argc, char** argv){
     ros::Publisher pub = nh.advertise<PointCloud>("pointcloud", 5);
     static tf2_ros::StaticTransformBroadcaster staticTrans;
 
-    // cloudMean<pcl::PointXYZI> cE(nh);
-    //nimbus::cloudEdit <pcl::PointXYZI> cloud_edit(nh);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr model(new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::io::loadPCDFile("/home/vishnu/ros_ws/catkin_nim_ws/src/nimbus_cloud/test/wCube.pcd", *model);
+
+    cloudMean<pcl::PointXYZI> cMean(nh);
     nimbus::cloudFeatures<pcl::PointXYZI, pcl::Normal> cFeature(nh);
     nimbus::cloudKeypoints<pcl::PointXYZI> cKeypoints(nh);
 
@@ -54,28 +57,60 @@ int main(int argc, char** argv){
     cameraPose.transform.rotation.z = q.z();
     cameraPose.transform.rotation.w = q.w();
     bool save = true;
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr model_keypoint (new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::PointCloud<pcl::Normal>::Ptr model_norm (new pcl::PointCloud<pcl::Normal>());
+    pcl::PointCloud<pcl::SHOT352>::Ptr model_descriptor (new pcl::PointCloud<pcl::SHOT352>());
+
     while (ros::ok())
     {
-        if(newCloud){
-            pcl::PointCloud<pcl::Normal>::Ptr scene_norm (new pcl::PointCloud<pcl::Normal>());
-            pcl::PointCloud<pcl::PointXYZI>::Ptr scene (new pcl::PointCloud<pcl::PointXYZI>());
-            pcl::PointCloud<pcl::PointXYZI>::Ptr scene_keypoint (new pcl::PointCloud<pcl::PointXYZI>());
-            pcl::PointCloud<pcl::SHOT352>::Ptr scene_descriptor (new pcl::PointCloud<pcl::SHOT352>());
-            for(int i = 0; i < blob.height * blob.width; i++){
-                pcl::PointXYZI temp;
-                temp.x = blob.points[i].x;
-                temp.y = blob.points[i].y;
-                temp.z = blob.points[i].z;
-                //temp.intensity = blob.points[i].intensity;
-                scene->points.push_back(temp);
-            }
-            scene->height =  blob.height;
-            scene->width = blob.width;
+        ROS_ERROR("Starting");
+        pcl::PointCloud<pcl::Normal>::Ptr scene_norm (new pcl::PointCloud<pcl::Normal>());
+        pcl::PointCloud<pcl::PointXYZI>::Ptr scene (new pcl::PointCloud<pcl::PointXYZI>());
+        pcl::PointCloud<pcl::PointXYZI>::Ptr scene_blob (new pcl::PointCloud<pcl::PointXYZI>());
+        pcl::PointCloud<pcl::PointXYZI>::Ptr scene_keypoint (new pcl::PointCloud<pcl::PointXYZI>());
+        pcl::PointCloud<pcl::SHOT352>::Ptr scene_descriptor (new pcl::PointCloud<pcl::SHOT352>());
+        if(newCloud && cMean.cloudQueue.size() < 2){
+            cMean.cloudQueue.enqueue(blob);
+            newCloud = false;
+        }else{
+            cMean.meanFilter(*scene_blob, blob.width, blob.height);
+            cFeature.remover(scene_blob, blob.width, blob.height, 0.7, 0.6, *scene);
             scene->is_dense = false;
+            model->is_dense = false;
+            
             cFeature.cloudNormalEstimationOMP(scene, *scene_norm);
+            cFeature.cloudNormalEstimationOMP(model, *model_norm);
             cKeypoints.cloudUniformSampling(scene, scene_keypoint);
+            cKeypoints.cloudUniformSampling(model, model_keypoint);
             cFeature.cloudSHOTEstimationOMP(scene_keypoint, scene_norm, scene, scene_descriptor);
+            cFeature.cloudSHOTEstimationOMP(model_keypoint, model_norm, model, model_descriptor);
+            ROS_ERROR("Size of model decriptor: %d", model_descriptor->points.size());
+             ROS_ERROR("Size of model decriptor: %d", scene_descriptor->size());
+
+            pcl::CorrespondencesPtr model_scene_corr (new pcl::Correspondences());
+            pcl::KdTreeFLANN<pcl::SHOT352> match_search;
+            match_search.setInputCloud(model_descriptor);
+            for(std::size_t i = 0; i < scene_descriptor->size(); ++i){
+                std::vector<int> neigh_indices(1);
+                std::vector<float> neigh_sqrt_distance(1);
+
+                if(! std::isfinite(scene_descriptor->at (i).descriptor[0])){
+                    ROS_ERROR("Scene descriptor at: %d --> %f", i, scene_descriptor->at (i).descriptor[0]);
+                    continue;
+                }
+                int found_neighs = match_search.nearestKSearch(scene_descriptor->at (i), 1, neigh_indices, neigh_sqrt_distance);
+                ROS_ERROR("Neighbooring sqrt dist: %f", neigh_sqrt_distance[0]);
+                if(found_neighs == 1 && neigh_sqrt_distance[0] < 0.25f){
+                    pcl::Correspondence corr(neigh_indices[0], static_cast<int> (i), neigh_sqrt_distance[0]);
+                    model_scene_corr->push_back(corr);
+                }
+            }
+            ROS_ERROR("Found Correnspondence is: %d", (int)model_scene_corr->size());
         }
+        scene_keypoint->header.frame_id= "detection";
+        pcl_conversions::toPCL(ros::Time::now(), scene_keypoint->header.stamp);
+        pub.publish(scene_keypoint);
         cameraPose.header.stamp = ros::Time::now();
         staticTrans.sendTransform(cameraPose);
         ros::spinOnce();
