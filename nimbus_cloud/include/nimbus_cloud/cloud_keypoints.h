2cd907ffa9c4e2b8828fd7adc0bdb6314b471b2a
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-04-01 15:18:36 +0200

    ISS Keypoint Extraction

--- nimbus_cloud/include/nimbus_cloud/cloud_keypoints.h
+++ nimbus_cloud/include/nimbus_cloud/cloud_keypoints.h
@@ -10,6 +10,9 @@
 #include <pcl/search/kdtree.h>
 #include <pcl/filters/uniform_sampling.h>
 
+#include <pcl/keypoints/iss_3d.h>
+#include <pcl/keypoints/impl/iss_3d.hpp>
+
 #include <nimbus_cloud/cloud_util.h>
 
 /** 
@@ -24,6 +27,7 @@ namespace nimbus{
             typedef pcl::PointCloud<PointInType> PointCloud;
             typedef boost::shared_ptr<PointCloud> PointCloudPtr;
             typedef boost::shared_ptr<const PointCloud> PointCloudConstPtr;
+            pcl::search::KdTree<pcl::PointXYZI>::Ptr tree;
         protected:
             typename pcl::PointCloud<PointInType>::Ptr keypointOut;
             double keypoint_sr;
@@ -42,6 +46,7 @@ namespace nimbus{
             * Detects Intrinstic Shape Sigmature keypoint for a given point cloud
             * Reconmmended to implement Filter before
            */
+          template <typename NormalType>
           void cloudISSKeypoints(const PointCloudConstPtr blob);
             
     };
@@ -54,7 +59,7 @@ nimbus::cloudKeypoints<PointInType>::~cloudKeypoints(){}
 template <class PointInType>
 void nimbus::cloudKeypoints<PointInType>::cloudUniformSampling(const PointCloudConstPtr blob){
     pcl::UniformSampling<PointInType> uniSampling;
-    // double searchRadius = this->computeCloudResolution(blob);
+    double searchRadius = this->computeCloudResolution(blob);
     // this->keypoint_sr *= searchRadius;
     uniSampling.setInputCloud(blob);
     uniSampling.setRadiusSearch(keypoint_sr);
@@ -63,9 +68,22 @@ void nimbus::cloudKeypoints<PointInType>::cloudUniformSampling(const PointCloudC
 }
 
 template <class PointInType>
+template <typename NormalType>
 void nimbus::cloudKeypoints<PointInType>::cloudISSKeypoints(const PointCloudConstPtr blob)
 {
-
+    double resolution = this->computeCloudResolution(blob);
+    tree.reset(new pcl::search::KdTree<pcl::PointXYZI> ());
+    pcl::ISSKeypoint3D<PointInType, PointInType, NormalType> iss;
+    iss.setSalientRadius(6 * resolution);
+    iss.setNonMaxRadius(4 * resolution);
+    iss.setMinNeighbors(5);
+    iss.setThreshold21(0.975);
+    iss.setThreshold32(0.975);
+    iss.setNumberOfThreads(4);
+    iss.setInputCloud(blob);
+    iss.setSearchMethod(tree);
+    keypointOut.reset(new pcl::PointCloud<PointInType>());
+    iss.compute(*keypointOut);
 }
 
 #endif
