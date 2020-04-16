75ef19e2dcbe530e7c9e2e8fa2886fd730597c56
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-04-16 11:42:05 +0200

    Subscriber to k4a

--- nimbus_cloud/src/nimbus_io.cpp
+++ nimbus_cloud/src/nimbus_io.cpp
@@ -21,7 +21,7 @@
 double remove_w, remove_h, z_max, z_min;
 bool save = false;
 
-typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 PointCloud cloud_blob;
 bool newCloud = false;
 void callback(const PointCloud::ConstPtr& msg){
@@ -45,13 +45,13 @@ void dynamicCallback(nimbus_cloud::cloudEditConfig &config, uint32_t data)
 int main(int argc, char** argv){
     ros::init(argc, argv, "nimbus_driver_io_node");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe<PointCloud>("/nimbus/pointcloud", 10, callback);
+    ros::Subscriber sub = nh.subscribe<PointCloud>("points2", 10, callback);
     ros::Subscriber subSave = nh.subscribe<std_msgs::Bool>("save_pointcloud", 10, saveCallback);
     ros::Publisher pub = nh.advertise<PointCloud>("pointcloud", 5);
     static tf2_ros::StaticTransformBroadcaster staticTrans;
 
-    cloudMean<pcl::PointXYZI> cE(nh);
-    nimbus::cloudEdit <pcl::PointXYZI> cloud_edit(nh);
+    cloudMean<pcl::PointXYZ> cE(nh);
+    nimbus::cloudEdit <pcl::PointXYZ> cloud_edit(nh);
 
     dynamic_reconfigure::Server<nimbus_cloud::cloudEditConfig> server;
     dynamic_reconfigure::Server<nimbus_cloud::cloudEditConfig>::CallbackType f;
@@ -60,7 +60,7 @@ int main(int argc, char** argv){
 
     geometry_msgs::TransformStamped cameraPose;
     cameraPose.child_frame_id = "Mcamera";
-    cameraPose.header.frame_id = "world";
+    cameraPose.header.frame_id = "camera_base";
     cameraPose.transform.translation.x = 0;
     cameraPose.transform.translation.y = 0;
     cameraPose.transform.translation.z = 1;
@@ -74,44 +74,40 @@ int main(int argc, char** argv){
     while (ros::ok())
     {
         if(newCloud){
-            if(cE.cloudQueue.size() < 20){
-                cE.cloudQueue.enqueue(cloud_blob);
-                newCloud = false;
+            
+            PointCloud::Ptr cloud(new PointCloud());
+            PointCloud::Ptr cloudE(new PointCloud());
+            PointCloud::Ptr cloudZ(new PointCloud());
+            pcl::copyPointCloud(cloud_blob, *cloud);
+            cloud_edit.remover(cloud, cloud_blob.width, cloud_blob.height, remove_w, remove_h, *cloudE);
+            float addZ = 0;
+            float counter = 0;
+            
+            cloud_edit.zRemover(cloudE, z_max, z_min, *cloudZ);
+            if(save == true){
+                ROS_INFO("Saving");
+                pcl::io::savePCDFile("model1.pcd", *cloudZ);
+                ROS_INFO("Saved");
+                save == false;
             }
-            else{
-                PointCloud::Ptr cloud(new PointCloud());
-                PointCloud::Ptr cloudE(new PointCloud());
-                PointCloud::Ptr cloudZ(new PointCloud());
-                cE.meanFilter (*cloud, cloud_blob.width, cloud_blob.height);
-                cloud_edit.remover(cloud, cloud_blob.width, cloud_blob.height, remove_w, remove_h, *cloudE);
-                float addZ = 0;
-                float counter = 0;
-                
-                cloud_edit.zRemover(cloudE, z_max, z_min, *cloudZ);
-                if(save == true){
-                    ROS_INFO("Saving");
-                    pcl::io::savePCDFile("model1.pcd", *cloudZ);
-                    ROS_INFO("Saved");
-                    save == false;
-                }
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////
-                // pcl::RangeImage::Ptr range_image (new pcl::RangeImage);
-                // cloud_edit._toRangeImage(cloudZ, *range_image);
-                // ROS_INFO("Range Image size: %d", (int)range_image->points.size());
-                // pcl::visualization::PCLVisualizer viewer("Range Image");
-                // viewer.setBackgroundColor(1,1,1);
-                // viewer.addCoordinateSystem(1.0f, "global");
-                // pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange> range_image_color_handler (range_image, 150, 150, 150);
-                // viewer.addPointCloud(range_image, range_image_color_handler, "range image");
-                // viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "range image");
-                // viewer.spin();
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////
-                cloudZ->header.frame_id= "Mcamera";
-                pcl_conversions::toPCL(ros::Time::now(), cloudZ->header.stamp);
-                pub.publish(cloudZ);
-                cloud.reset();
-            }
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // pcl::RangeImage::Ptr range_image (new pcl::RangeImage);
+            // cloud_edit._toRangeImage(cloudZ, *range_image);
+            // ROS_INFO("Range Image size: %d", (int)range_image->points.size());
+            // pcl::visualization::PCLVisualizer viewer("Range Image");
+            // viewer.setBackgroundColor(1,1,1);
+            // viewer.addCoordinateSystem(1.0f, "global");
+            // pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange> range_image_color_handler (range_image, 150, 150, 150);
+            // viewer.addPointCloud(range_image, range_image_color_handler, "range image");
+            // viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "range image");
+            // viewer.spin();
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////
+            cloud->header.frame_id= "Mcamera";
+            pcl_conversions::toPCL(ros::Time::now(), cloud->header.stamp);
+            pub.publish(cloud);
+            cloud.reset();
             // cloud_blob.points.clear()
+            newCloud = false;
         }
         cameraPose.header.stamp = ros::Time::now();
         staticTrans.sendTransform(cameraPose);
