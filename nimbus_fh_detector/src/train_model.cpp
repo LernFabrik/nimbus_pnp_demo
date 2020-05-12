4f9d997f618bf8c494a8c03a32af0f3a43baf074
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-05-12 16:51:59 +0200

    Model ground truth correction

--- nimbus_fh_detector/src/train_model.cpp
+++ nimbus_fh_detector/src/train_model.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <boost/filesystem.hpp>
 
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
@@ -20,11 +21,14 @@ class ModelTraining : public cloudUtilities<pcl::PointXYZI>
 {
     protected:
         typename pcl::PointCloud<pcl::PointXYZI>::Ptr _cloud;
+        typename pcl::PointCloud<pcl::PointXYZI>::Ptr _ground;
+        typename pcl::PointCloud<pcl::PointXYZI>::Ptr _model;
         typename pcl::PointCloud<pcl::PointXYZI>::Ptr blob;
         typename pcl::PointCloud<pcl::PointXYZI>::Ptr blob_removed;
         bool save_point_cloud;
         ros::NodeHandle _nh;
         ros::Subscriber _sub;
+        ros::Subscriber _sub_save;
         ros::Publisher _pub;
         tf2_ros::StaticTransformBroadcaster _staticTrans;
         geometry_msgs::TransformStamped _cameraPose;
@@ -36,6 +40,7 @@ class ModelTraining : public cloudUtilities<pcl::PointXYZI>
         {
             _sub = _nh.subscribe<sensor_msgs::PointCloud2>("/nimbus/pointcloud", 10 , boost::bind(&ModelTraining::Callback, this, _1));
             _pub = _nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("filtered_cloud", 5);
+            _sub_save = nh.subscribe<std_msgs::Bool>("save_pointcloud", 10, boost::bind(&ModelTraining::saveCallback, this, _1));
             _cameraPose.child_frame_id = "detector";
             _cameraPose.header.frame_id = "world";
             _cameraPose.transform.translation.x = 0;
@@ -60,18 +65,47 @@ class ModelTraining : public cloudUtilities<pcl::PointXYZI>
             this->_queue.enqueue(*blob_removed);
         }
 
+        void saveCallback(const std_msgs::Bool::ConstPtr &msg)
+        {
+            save_point_cloud = msg->data;
+        }
+
         void run()
         {
+            std::string model_name = "box_";
+            std::string extention = ".pcd";
+            std::string saved_groudtruth;
+            int counter = 0;
             while(ros::ok())
             {
                 unsigned int queue_size = this->_queue.size();
                 if(queue_size > 10)
                 {
                     _cloud.reset(new pcl::PointCloud<pcl::PointXYZI>());
+                    _ground.reset(new pcl::PointCloud<pcl::PointXYZI>());
+                    _model.reset(new pcl::PointCloud<pcl::PointXYZI>());
                     this->meanFilter(*_cloud);
+
+                    if(save_point_cloud){
+                        save_point_cloud = false;
+                        std::stringstream ss;
+                        ss << model_name;
+                        if (counter == 0){
+                            ss << "1" << extention;
+                            saved_groudtruth = ss.str();
+                            pcl::io::savePCDFileASCII(ss.str(), *_cloud);
+                            counter += 1;
+                        }else{
+                            pcl::io::loadPCDFile(saved_groudtruth, *_ground);
+                            //this->modelFromGroudtruth(_ground, _cloud, 0.03, *_model);
+                        }
+                        ROS_ERROR("Saved");
+                    }
+
                     _cloud->header.frame_id = "detector";
                     pcl_conversions::toPCL(ros::Time::now(), _cloud->header.stamp);
                     _pub.publish(_cloud);
+
                     if(queue_size > 100){
                         while(!this->_queue.isEmpty()){
                             this->_queue.dequeue(*_cloud);
