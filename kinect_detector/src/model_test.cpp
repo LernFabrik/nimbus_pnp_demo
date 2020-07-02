0ebe78dffccc1a8c604e976da77869851d21a233
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-07-02 14:32:24 +0200

    Possible solution to - [ x ] TODO Save upon key stroke

--- kinect_detector/src/model_test.cpp
+++ kinect_detector/src/model_test.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <thread>
 #include <pthread.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
+#include <iostream>
 
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -26,6 +33,49 @@ static void* user_thread(void*)
         }
         
     }    
+}
+
+void disable_canonical(){
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+}
+void enable_canonical(){
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+}
+bool key_pressed(char c){
+    struct pollfd fds[1];
+    fds[0].fd = 0;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+    int r = poll(fds, 1, 1);
+    if(r > 0){
+        if(fds[0].revents & POLLIN || fds[0].revents & POLLRDBAND || fds[0].revents & POLLRDNORM){
+            char buffer[1024];
+            memset(buffer, 0, sizeof(buffer));
+            int n = read(0, buffer, sizeof(buffer) -1);
+            for(int i = 0; i < n; ++i){
+                if(buffer[i] == c){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 int main(int argc, char** argv){
@@ -68,18 +118,17 @@ int main(int argc, char** argv){
     {
         pcl_conversions::toPCL(ros::Time::now(), cloud->header.stamp);
         pub.publish(cloud);
-
         r.sleep();
-
         //TODO Save upon key stroke
-        ROS_INFO("Saving Point Cloud");
-        pcl::io::savePCDFile("/home/vishnu/ros_ws/test/box_modif.pcd", *cloud);
+        if(key_pressed('x'))
+        {
+            ROS_INFO("Saving Point Cloud");
+            pcl::io::savePCDFile("/home/vishnu/ros_ws/test/box_modif.pcd", *cloud);
+        }
     }
 
     // (void) pthread_join(key_thread, NULL);
     // ROS_INFO("Saving Point Cloud");
     // pcl::io::savePCDFile("/home/vishnu/ros_ws/test/box_modif.pcd", *cloud);
-
-
     return 0;
 }
