80a48184073e2ffc96462d099cae533940a20087
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-07-07 16:37:59 +0200

    Detect Box

---
+++ box_detector/include/box_detector/box_detector.hpp
@@ -0,0 +1,50 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2020 IWT Wirtschaft und Technik GmbH
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * @file box_detector.hpp
+ * @author Vishnuprasad Prachandabhanu (vishnu.pbhat93@gmail.com)
+ */
+
+#pragma once
+
+#include <ros/ros.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/pcd_io.h>
+#include <boost/foreach.hpp>
+
+namespace nimbus
+{
+    class BoxDetector
+    {
+        private:
+        protected:
+        public:
+            BoxDetector(ros::NodeHandle nh);
+            ~BoxDetector();
+            void zAxisLimiter(const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZ>> &blob);
+            void box3DCentroid(const boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZ>> &blob);
+    };
+
+} // namespace nimbus
